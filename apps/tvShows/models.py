1754baa0e3e4cf6e8c00f854640655d4904654fd
Fabian Carvajal <79953110+FabianCarvajalPino@users.noreply.github.com>
2021-05-19 18:54:59 -0400

    actualizado con validadores de cantidad de caracteres al crear nuevos shows

--- apps/tvShows/models.py
+++ apps/tvShows/models.py
@@ -1,6 +1,22 @@
+#from __future__ import unicode_literals
 from django.db import models
 
+
 # Create your models here.
+
+class ShowManager(models.Manager):
+    def basic_validator(self, postData):
+        errors = {}
+        print(postData)
+        if len(postData['title']) < 2:
+            errors['title'] = "El titulo deberia tener al menos 2 caracteres"
+        if len(postData['network']) < 3:
+            errors['network'] = "El network debe tener al menos 3 caracteres"
+        
+        if len(postData['description']) < 10:
+            errors['desc'] = "La descripción debe tener al menos 10 caracteres"
+        return errors
+
 class Show(models.Model):
     title = models.CharField(max_length=255)
     network = models.CharField(max_length=255)
@@ -8,3 +24,4 @@ class Show(models.Model):
     release_date = models.DateField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    objects = ShowManager()
--- apps/tvShows/views.py
+++ apps/tvShows/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, HttpResponse
 from .models import Show
+from django.contrib import messages
 
 # Create your views here.
 def root(request):
@@ -9,14 +10,23 @@ def new_show(request):
     return render(request, "newShow.html")
 
 def create(request):
-    Show.objects.create(
-        title = request.POST['title'],
-        network = request.POST['network'],
-        desc = request.POST['description'],
-        release_date = request.POST['rls_date']
-    )
-    idShow = Show.objects.last().id
-    return redirect(f'/shows/{idShow}')
+    
+    errors = Show.objects.basic_validator(request.POST)
+    if len(errors) > 0:
+        for key, value in errors.items():
+            messages.error(request, value)
+        return redirect("/shows/new")
+    else:
+    
+        Show.objects.create(
+            title = request.POST['title'],
+            network = request.POST['network'],
+            desc = request.POST['description'],
+            release_date = request.POST['rls_date']
+        )
+        messages.success(request, 'Show agregado exitosamente')
+        idShow = Show.objects.last().id
+        return redirect(f'/shows/{idShow}')
 
 def show(request, idShow):
     show = Show.objects.get(id = int(idShow))
@@ -40,13 +50,28 @@ def edit(request, idShow):
     return render(request, 'editShow.html', context)
 
 def update(request, idShow):
-    show_to_update = Show.objects.get(id=int(idShow))
-    show_to_update.title = request.POST['title']
-    show_to_update.network = request.POST['network']
-    show_to_update.desc = request.POST['description']
-    show_to_update.release_date = request.POST['rls_date']
-    show_to_update.save()
-    return redirect(f'/shows/{idShow}')
+    errors = Show.objects.basic_validator(request.POST)
+    if len(errors) > 0:
+        for key,value in errors.items():
+            messages.error(request, value)
+        return redirect(f'/shows/{idShow}/edit')
+        #return render(request, 'editShow.html')
+        '''
+        errors = Show.objects.basic_validator(request.POST)
+    if len(errors) > 0:
+        for key, value in errors.items():
+            messages.error(request, value)
+        return redirect("/shows/new")
+    else:'''
+    else:
+        show_to_update = Show.objects.get(id=int(idShow))
+        show_to_update.title = request.POST['title']
+        show_to_update.network = request.POST['network']
+        show_to_update.desc = request.POST['description']
+        show_to_update.release_date = request.POST['rls_date']
+        show_to_update.save()
+        messages.success(request, "Show actualizado correctamente")
+        return redirect(f'/shows/{idShow}')
 
 def destroy(request, idShow):
     show_to_delete = Show.objects.get(id=int(idShow))
